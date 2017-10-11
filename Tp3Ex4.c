acbcc4c2da7546c354875d6269ec3bc1f719b1a8
Dhia ben maati <32402326+dhiabm@users.noreply.github.com>
2017-10-11 21:28:07 +0200

    Update Tp3Ex4.c

--- Tp3Ex4.c
+++ Tp3Ex4.c
@@ -1,29 +1,19 @@
-/*
- ============================================================================
- Name        : Tp3Ex4.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	int i,somme,j;
-	for (i=1;i<1000;i++)
-	{somme=0;
-		for (j=1;j<=i/2;j++){
-		if (i%j ==0)
-		{
-			somme=somme+j;
-		}
-		}
-		if (somme ==i)
-		{
-			printf ("%d est parfait",i);
-		}
-	}
-}
+int main()
+{
+ int i,s,j;
+
+ for (i=1;i<=1000;i++){
+    s=0;
+    for(j=1;j<=i/2;j++){
+    if (i%j==0) {
+        s=s+j;
+    }}
+    if (s==i)
+    {
+        printf("%d est un nb parfait \n",i);
+    }}
+
+    return 0;
